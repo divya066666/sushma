048902770f22b8870aee1b277d8f330e4691cceb
divya066666 <42571843+divya066666@users.noreply.github.com>
2018-09-04 11:45:53 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,7 @@
+1='a'
+if 1 in('a','e','i','o','u'):
+     print("%s is a vowel".%1)
+elif 1 =='y':
+     print("sometimes letter y stands for vowel,sometimes stands for consonants")
+else:
+     print("%s is a consonant".%1)
