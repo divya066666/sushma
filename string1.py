f72b451b1557d83c9d799bd2dd1b14c9463440f6
divya066666 <42571843+divya066666@users.noreply.github.com>
2018-11-29 11:24:26 +0530

    Create string1.py

---
+++ string1.py
@@ -0,0 +1,15 @@
+def isNumber(s) : 
+      
+    for i in range(len(s)) : 
+        if s[i].isdigit() != True : 
+            return False
+            return True
+if __name__ == "__main__" : 
+    str  = "6790"
+  
+    if isNumber(str) : 
+        print("Integer") 
+  
+    else : 
+        print("yes")
+  
